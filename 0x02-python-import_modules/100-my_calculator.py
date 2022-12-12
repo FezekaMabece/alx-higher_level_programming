ed012d7519182a3e6ae58bd217261e901378b24a
FezekaMabece <fmabece@gmail.com>
2022-12-12 13:50:53 -0800

    correcting ModuleNotFoundError

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-from calculaor_1 import add, sub, mul, div
-from sys import argv
-
-def main():
-    if len(argv) != 4:
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
 
-    if argv[2] not in '+-*/':
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    c = sys.argv[2]
+    if c == '+':
+        print('{} + {} = {}'.format(a, b, add(a, b)))
+    elif c == '-':
+        print('{} - {} = {}'.format(a, b, sub(a, b)))
+    elif c == '*':
+        print('{} * {} = {}'.format(a, b, mul(a, b)))
+    elif c == '/':
+        print('{} / {} = {}'.format(a, b, sub(a, b)))
+    else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
-
-    a = int(arv[1])
-    b = int(argv[3])
-    if argv[2] == '+':
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-    elif argv[2] == '-':
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-    elif argv[2] == '*':
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-    elif argv[2] == '/':
-        print('{} / {} = {}'.format(a, b, sub(a, b)))
-
-if __name__ == "__main__":
-    main()
