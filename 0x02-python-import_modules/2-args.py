346065712599c230cc5596ed1a7d8e535ad82b03
FezekaMabece <fmabece@gmail.com>
2022-12-12 12:45:58 -0800

    3rd attempt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-    l = len(sys.argv) - 1
-    print("{} argument{}{}"
-            .format(l, "" if l == 1 else "s", "." if l == 0 else ":"))
-    for i in range(l):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    """Prints the argument list passed to the program
+
+    The program takes all the arguments starting from the second
+    and prints the number of arguments and their value
+
+    """
+    a = sys.argv
+    b = len(a) - 1
+
+    if b > 1:
+        print(b, "arguments:")
+        for i in range(1, b + 1):
+            print("{:d}: {}".format(i, a[i]))
+    elif b == 1:
+        print(b, "arguments:")
+        for i in range(1, b + 1):
+            print("{:d}: {}".format(i, a[i]))
+    elif b == 0:
+        print(b, "arguments.")
