68e0d25cc8aed5a73c6058a74cfb868b35166363
FezekaMabece <fmabece@gmail.com>
2022-12-15 10:25:03 -0800

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    b_dictionary = a_dictionary
+
+    for a, b in b_dictionary.items():
+        b_dictionary[k] = b * 2
+
+    return b_dictionary
