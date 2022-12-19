dd2ccfdb3d099d2c3f6daf8a97536ef9218408fd
FezekaMabece <fmabece@gmail.com>
2022-12-19 09:06:02 -0800

    0. Safe list print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    my_list = ['F','e','z','e','k','a']
+
+while True:
+    try:
+        my_list
+    except:
+        print("This is not my list")
+    else:
+        break
+print(f"{my_list")
